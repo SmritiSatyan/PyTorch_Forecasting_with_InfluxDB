08761a57cd3f9a578288d20b1323e8dea36649da
Smriti S <35428245+SmritiSatyan@users.noreply.github.com>
2022-04-09 16:38:39 +0530

    Update PyTorch_InfluxDB.py

--- PyTorch_InfluxDB.py
+++ PyTorch_InfluxDB.py
@@ -35,25 +35,49 @@ bucket = "bucket"
 # The url depends on the location selected during sign up
 url = "https://us-east-1-1.aws.cloud2.influxdata.com"
     
-with InfluxDBClient(url=url, token=token, org=org) as client:
-    """
-    Ingest DataFrame with default tags
-    """
-    point_settings = PointSettings(**{"type": "stallion_df"})
-    point_settings.add_default_tag("stallion-data", "ingest-data-frame")
+import pandas as pd
     
-    write_api = client.write_api(write_options=SYNCHRONOUS, point_settings=point_settings)
-    write_api.write(bucket="bucket1", record=data, data_frame_measurement_name="financial-analysis-df")
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS, PointSettings
+    
+from pytorch_forecasting.data.examples import get_stallion_data
+import numpy as np
+stallion_df = get_stallion_data()
+token = "generated token"
+org = "email"
+url = "https://us-east-1-1.aws.cloud2.influxdata.com"
+    
+# using line protocol to write data to InfluxDB cloud
+client = InfluxDBClient(url=url, token=token, org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+    
+# line protocol should have the syntax
+'''<measurement>[,<tag_key>=<tag_value>[,<tag_key>=<tag_value>]] <field_key>=<field_value>[,<field_key>=<field_value>] [<timestamp>]''''
+    
+# add the columns required for the prediction
+data = "beverage_sales,volume=59.315,date=2013-01-01,timeseries=166 actual_price=52.212"
+write_api.write(bucket, org, data)
+    
 
-    """
-    Querying ingested data
-    """
-    query = 'from(bucket:"bucket1")' \
-            ' |> range(start: -2d, stop: now())' \
-            ' |> filter(fn: (r) => r._measurement == "stallion_df")' 
-            
-client = InfluxDBClient(url=url, token=token, org= org, debug=False)
-stallion_df = client.query_api().query_data_frame(org= org, query=query)
+# Querying ingested data
+results = [] # initialize an empty list to append records
+
+with InfluxDBClient(url="https://us-east-1-1.aws.cloud2.influxdata.com", token=token, org=org) as client:
+    query = """option v = {timeRangeStart: -1h, timeRangeStop: now()}
+                        from(bucket: "my-bucket-1")
+                        |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
+                        |> group(columns: ["beverage_sales"])
+                        |> filter(fn: (r) => r["_field"] == "actual_price")"""
+    tables = client.query_api().query(query, org=org)
+    for table in tables:
+        for record in table.records:
+            results.append((record.get_field(), 
+                                record.get_value(), 
+                                record.get_measurement(), 
+                                record.get_time()))
+                        
+# convert the list to a dataframe                        
+stallion_df = pd.DataFrame(results, columns=['_field', '_value', '_measurement', other-columns])
 
 # Add time index
 stallion_df["time_idx"] = stallion_df["date"].dt.year * 12 + stallion_df["date"].dt.month
@@ -199,7 +223,7 @@ trainer = pl.Trainer(
     logger=logger,
 )
 
-
+# using the `TemporalFusionTransformer` to fit the data
 tft = TemporalFusionTransformer.from_dataset(
     training,
     learning_rate=0.03,
