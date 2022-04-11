53c2380ebefc30c3d92c83e0eea7c5ba6986cca7
Smriti S <35428245+SmritiSatyan@users.noreply.github.com>
2022-04-11 21:04:34 +0530

    Update ingest_and_query.py

--- ingest_and_query.py
+++ ingest_and_query.py
@@ -10,27 +10,53 @@ token = "generated token"
 org = "email"
 url = "https://us-east-1-1.aws.cloud2.influxdata.com"
     
-# using line protocol to write data to InfluxDB cloud
-client = InfluxDBClient(url=url, token=token, org=org)
-write_api = client.write_api(write_options=SYNCHRONOUS)
-    
-# line protocol should have the syntax
-'''<measurement>[,<tag_key>=<tag_value>[,<tag_key>=<tag_value>]] <field_key>=<field_value>[,<field_key>=<field_value>] [<timestamp>]''''
-    
-# add the columns required for the prediction
-data = "beverage_sales,volume=59.315,date=2013-01-01,timeseries=166 actual_price=52.212"
-write_api.write(bucket, org, data)
-    
+# pushing contents of dataframe to InfluxDB cloud
+stallion_data = get_stallion_data()
+stallion_data = stallion_data.rename(columns={"date": "time"})
+stallion_data = stallion_data.set_index('time')
+        
+"""
+Enable logging for DataFrame serializer
+"""
+loggerSerializer = logging.getLogger('influxdb_client.client.write.dataframe_serializer')
+loggerSerializer.setLevel(level=logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s | %(message)s'))
+loggerSerializer.addHandler(handler)
 
-# Querying ingested data
-results = [] # initialize an empty list to append records
+"""
+Ingest DataFrame
+"""
+print()
+print("=== Ingesting DataFrame via batching API ===")
+print()
+startTime = datetime.now()
+
+
+with InfluxDBClient(url=url, token=token, org=org) as client:
+
+    """
+    Use batching API
+    """
+    with client.write_api() as write_api:
+        write_api.write(bucket='myBucket', record=stallion_data,
+                        data_frame_tag_columns=['agency', 'sku'],
+                        data_frame_measurement_name="stallion_data")
+        print()
+        print("Wait to finishing ingesting DataFrame...")
+        print()
+
+print()
+print(f'Import finished in: {datetime.now() - startTime}')
+print()
+
+results = []
 
 with InfluxDBClient(url="https://us-east-1-1.aws.cloud2.influxdata.com", token=token, org=org) as client:
-    query = """option v = {timeRangeStart: -1h, timeRangeStop: now()}
-                        from(bucket: "my-bucket-1")
+    query = """option v = {timeRangeStart: -10h, timeRangeStop: now()}
+                        from(bucket: "myBucket")
                         |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
-                        |> group(columns: ["beverage_sales"])
-                        |> filter(fn: (r) => r["_field"] == "actual_price")"""
+                        |> group(columns: ["stallion_data"])"""
     tables = client.query_api().query(query, org=org)
     for table in tables:
         for record in table.records:
@@ -40,6 +66,4 @@ with InfluxDBClient(url="https://us-east-1-1.aws.cloud2.influxdata.com", token=t
                                 record.get_time()))
                         
 # convert the list to a dataframe                        
-stallion_df = pd.DataFrame(results, columns=['_field', '_value', '_measurement', other-columns])
-
-Note: You can use ``record.values.get("column-name")`` if you wish to fetch other columns from InfluxDB.
+influx_df = pd.DataFrame(results, columns=['_field', '_value', '_measurement', column-names])
