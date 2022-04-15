6758754f4b91abeb560561812e5d41dd259770da
Smriti S <35428245+SmritiSatyan@users.noreply.github.com>
2022-04-16 01:02:56 +0530

    Update sample_influxDB_connection.py

--- sample_influxDB_connection.py
+++ sample_influxDB_connection.py
@@ -1,15 +1,14 @@
-    import influxdb_client
-    from influxdb_client import InfluxDBClient, Point, WritePrecision
-    from influxdb_client.client.write_api import SYNCHRONOUS
+import influxdb_client
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-    #  Generate an API token as shown in step 2 above
-    # Fill in the below attributes after creating an account on InfluxDB Cloud
-    token = " "
-    org = " "
-    bucket = " "
-    # URL depends on the region selected during sign up
-    url="https://europe-west1-1.gcp.cloud2.influxdata.com" 
-    client = InfluxDBClient(url=url, token=token, org=org,debug=True)
-    '''
-    flux query execution/other source code
-    '''
+# Fill in the below attributes after creating an account on InfluxDB Cloud
+token = "YOUR_TOKEN_HERE"
+org = "YOUR_EMAIL_HERE"
+bucket = "YOUR_BUCKET_HERE"
+# URL depends on the region selected during sign up, an example is <url= "https://europe-west1-1.gcp.cloud2.influxdata.com">
+url = "YOUR_URL_HERE"
+client = InfluxDBClient(url=url, token=token, org=org, debug=True)
+"""
+Flux query execution/other source code
+"""
